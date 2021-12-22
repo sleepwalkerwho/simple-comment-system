ae85584a1a5078df33817ab8e9c1dcf83344e135
SleepWho <sleepwalker@gmail.com>
2021-12-22 22:42:32 +0300

    release-1.1 added form

--- front/itmo-hw3/src/component/CommentPage.tsx
+++ front/itmo-hw3/src/component/CommentPage.tsx
@@ -1,4 +1,5 @@
 import React, {Component, ReactNode} from "react";
+import { Button, TextField } from "@material-ui/core";
 import "./CommentPage.scss"
 
 export class CommentPage extends Component {
@@ -6,7 +7,9 @@ export class CommentPage extends Component {
     public render(): ReactNode {
         return (
             <div className="div-comments-page">
-                ...
+                <TextField label="Nickname"/>
+                <TextField label="Comment"/>
+                <Button>Send Comment</Button>
             </div>
         )
     }
